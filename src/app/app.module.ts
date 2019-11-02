0646f0922694e98d0f3764c4e2c25b302dea48a6
Ariella Wills <Ariellaw@gmail.com>
2019-11-02 12:29:41 +0200

    added letter-box component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PalindromEditorComponent } from './palindrom-editor/palindrom-editor.component';
+import { LetterBoxComponent } from './letter-box/letter-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PalindromEditorComponent
+    PalindromEditorComponent,
+    LetterBoxComponent
   ],
   imports: [
     BrowserModule,
