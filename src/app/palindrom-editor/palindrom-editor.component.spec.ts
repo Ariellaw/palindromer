9a1cea3fd8b97dd25201aee219332914364f4726
Eyal Pinkas <eyal@next-insurance.com>
2020-03-07 20:18:08 +0200

    attempt to add basic test

--- src/app/palindrom-editor/palindrom-editor.component.spec.ts
+++ src/app/palindrom-editor/palindrom-editor.component.spec.ts
@@ -3,7 +3,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { PalindromEditorComponent } from './palindrom-editor.component';
 import {FormsModule} from '@angular/forms';
 import {LetterBoxComponent} from '../letter-box/letter-box.component';
-import {NavbarComponent} from '../navbar/navbar.component';
 import {PivotLetterComponent} from '../pivot-letter/pivot-letter.component';
 
 describe('PalindromEditorComponent', () => {
@@ -30,5 +29,20 @@ describe('PalindromEditorComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain "tacocat" when first loaded', () => {
+    fixture.detectChanges();
+
+    let compiled = fixture.nativeElement;
+
+    //get text content of the palindrom editor
+    // let lettersInputs = compiled.querySelectorAll('input'); //this should work, but it doesn't for some reason, so I do this instead:
+    // let lettersInputs = document.querySelectorAll('#palindrom-container  input');
+    let lettersInputs = compiled.querySelectorAll('#palindrom-container  input');
+    let palindromContent = "";
+    lettersInputs.forEach( inp => palindromContent += inp.value)
+
+    expect(palindromContent).toBe('tacocat');
   });
 });
